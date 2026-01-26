f66e70bbfaf441305232736c906fcad3e2d6ccac
Stefan Seunarine <seunarine.stefan.lt@gmail.com>
2026-01-26 09:22:39 -0400

    Bump version to 1.0.50 and adjust height calculation in PDF rendering to add a fixed value

--- src/PDFLibExtended.js
+++ src/PDFLibExtended.js
@@ -656,7 +656,7 @@ class PDFLibExtended {
                 x: x,
                 y: y - ((defaultOptions.height) ? defaultOptions.height : drawParagraphResponse.height) + defaultOptions.size + defaultOptions.padding,
                 width: width,
-                height: (defaultOptions.height) ? defaultOptions.height : drawParagraphResponse.height - defaultOptions.padding,
+                height: (defaultOptions.height) ? defaultOptions.height : drawParagraphResponse.height + 100,
                 color: defaultOptions.backgroundColor,
             });
         }
